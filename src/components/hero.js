e185626f3ad1fdbbb59668416688b4efd7eae680
sitebymike <mike@sitebymike.com>
2022-02-08 14:15:35 -0600

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ import * as styles from './hero.module.css'
 const Hero = ({ image, title, content }) => (
   <div className={styles.hero}>
     {image && (
-      <GatsbyImage className={styles.image} alt={image} image={image} />
+      <GatsbyImage className={styles.image} alt={title} image={image} />
     )}
     <div className={styles.details}>
       <h1 className={styles.title}>{title}</h1>
